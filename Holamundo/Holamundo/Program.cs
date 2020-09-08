dafaefe4995e20f880208852c2e03a6daa3ee092
Alexander <ing.anchicoque@gmail.com>
2020-09-08 15:49:32 -0500

    Hola mundo

---
+++ Holamundo/Holamundo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo 08-09-2020");
+        }
+    }
+}
